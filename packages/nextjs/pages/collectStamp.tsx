8e0033dedacf9c80857ad72af6cbc663bc00a548
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2023-04-09 03:36:25 +0530

    get countryname

--- packages/nextjs/pages/collectStamp.tsx
+++ packages/nextjs/pages/collectStamp.tsx
@@ -5,7 +5,20 @@ const CollectStamp = () => {
   const [geoLocation, setGeoLocation] = useState<{
     latitude: number;
     longitude: number;
+    countryName: string;
   }>();
+
+  const fetchCountryName = async (latitude: number, longitude: number) => {
+    const apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&zoom=18&addressdetails=1`;
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new Error("Network error occurred while fetching country name.");
+    }
+
+    const data = await response.json();
+    return data.address && data.address.country;
+  };
 
   const handleFileUpload = async (event: any) => {
     const file = event.target.files[0];
@@ -15,12 +28,14 @@ const CollectStamp = () => {
         const exifData = await exifr.gps(file);
 
         if (exifData && exifData.latitude && exifData.longitude) {
-          setGeoLocation({ latitude: exifData.latitude, longitude: exifData.longitude });
+          const { latitude, longitude } = exifData;
+          const countryName = await fetchCountryName(latitude, longitude);
+          setGeoLocation({ latitude, longitude, countryName });
         } else {
           alert("No geo-location data found in this image.");
         }
       } catch (error) {
-        console.error("Error reading Exif data:", error);
+        console.error("Error reading Exif data or fetching country name:", error);
       }
     }
   };
@@ -40,7 +55,7 @@ const CollectStamp = () => {
       <input type="file" onChange={handleFileUpload} />
       {geoLocation && (
         <p>
-          Latitude: {geoLocation.latitude}, Longitude: {geoLocation.longitude}
+          Latitude: {geoLocation.latitude}, Longitude: {geoLocation.longitude}, country: {geoLocation.countryName}
         </p>
       )}
     </div>
